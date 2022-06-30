90a2c611336a0b361462e658a01763f4c178f299
MykolaZhura <nikoyea14@gmail.com>
2022-06-30 12:57:17 +0200

    refactor with new form hook

--- assets/installer/Views/InstallerSteps/Base/Base.jsx
+++ assets/installer/Views/InstallerSteps/Base/Base.jsx
@@ -1,90 +1,33 @@
 import React, { useEffect, useState } from 'react'
+import { useForm } from '../../../hooks/hooks'
 import apiService from '../../../utils/apiService'
 import WindowCard from '../../../Components/WindowCard/WindowCard'
 import Spinner from '../../../Components/Spiner/Spinner'
 import { Box, Button, Collapse, TextField, } from '@mui/material'
-import styles from '../styles.scss'
+import styles from './styles.scss'
 
 const Base = ({handleNextStep, step}) => {
   const [isLoading, setIsLoading] = useState(true)
-  const [stepData, setStepData] = useState({
-    data_required: true,
-    base_adserver_name: '',
-    base_domain: '',
-    base_support_email: '',
-    base_contact_email: '',
-    base_adpanel_host_prefix: '',
-    base_adserver_host_prefix: '',
-    base_aduser_host_prefix: '',
-  })
-  const [formErrors, setFormErrors] = useState({
-    base_adserver_name: '',
-    base_domain: '',
-    base_support_email: '',
-    base_contact_email: '',
-    base_adpanel_host_prefix: '',
-    base_adserver_host_prefix: '',
-    base_aduser_host_prefix: '',
-  })
-  const [isFormValid, setIsFormValid] = useState(false)
   const [showAdvancedOptions, setShowAdvancedOptions] = useState(false)
+  const {fields, errorObj, setFields, isFormValid, onFormChange, validate} = useForm({
+    base_adpanel_host_prefix: '',
+    base_adserver_host_prefix: '',
+    base_adserver_name: '',
+    base_aduser_host_prefix: '',
+    base_contact_email: '',
+    base_domain: '',
+    base_support_email: '',
+  })
 
   useEffect(() => {
     getStepData().catch(error => console.log(error))
   },[])
 
-  useEffect(() => {
-    checkIsFormValid()
-  }, [stepData, formErrors])
-
   const getStepData = async () => {
     setIsLoading(true)
     const response = await apiService.getCurrentStepData(step.path)
     setIsLoading(false)
-    setStepData({...stepData, ...response})
-  }
-
-  const checkIsFormValid = () => {
-    const isEmptyFields = Object.keys(stepData).some(el => {
-      if(el === 'data_required'){
-        return
-      }
-      return !stepData[el]
-    })
-    const isFormErrors = Object.keys(formErrors).some(el => !!formErrors[el])
-    setIsFormValid(!isEmptyFields && !isFormErrors)
-  }
-
-  const validate = (target) => {
-    const {name, value} = target
-    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g
-    const errors = {}
-
-    if(!name){
-      return
-    }
-
-    if(!value) {
-      errors[name] = 'Required field'
-    }
-    else if(name === 'base_support_email' || name === 'base_contact_email') {
-      if(emailRegex.test(value)){
-        errors[name] = ''
-      }
-      else {
-        errors[name] = 'Field must be an email'
-      }
-    } else {
-      errors[name] = ''
-    }
-
-    setFormErrors({ ...formErrors, ...errors })
-  }
-
-  const onFormChange = (e) => {
-    const {name, value} = e.target
-    setStepData({...stepData, [name]: value})
-    validate(e.target)
+    setFields({...fields, ...response})
   }
 
   const handleSubmit = async () => {
@@ -92,7 +35,7 @@ const Base = ({handleNextStep, step}) => {
       return
     }
     setIsLoading(true)
-    await apiService.sendStepData(step.path, stepData)
+    await apiService.sendStepData(step.path, fields)
     handleNextStep(step)
     setIsLoading(false)
   }
@@ -116,23 +59,23 @@ const Base = ({handleNextStep, step}) => {
             <Box className={styles.formBlock}>
               <TextField
                 className={styles.textField}
-                error={!!formErrors.base_adserver_name}
-                helperText={formErrors.base_adserver_name}
+                error={!!errorObj.base_adserver_name}
+                helperText={errorObj.base_adserver_name}
                 size="small"
                 name="base_adserver_name"
                 label="Adserver name"
-                value={stepData.base_adserver_name}
+                value={fields.base_adserver_name}
                 type="text"
                 required
               />
               <TextField
                 className={styles.textField}
-                error={!!formErrors.base_domain}
-                helperText={formErrors.base_domain}
+                error={!!errorObj.base_domain}
+                helperText={errorObj.base_domain}
                 size="small"
                 name="base_domain"
                 label="Domain name"
-                value={stepData.base_domain}
+                value={fields.base_domain}
                 type="text"
                 required
               />
@@ -140,24 +83,24 @@ const Base = ({handleNextStep, step}) => {
             <Box className={styles.formBlock}>
               <TextField
                 className={styles.textField}
-                error={!!formErrors.base_support_email}
-                helperText={formErrors.base_support_email}
+                error={!!errorObj.base_support_email}
+                helperText={errorObj.base_support_email}
                 size="small"
                 name="base_support_email"
                 label="Email to support"
-                value={stepData.base_support_email}
+                value={fields.base_support_email}
                 type="email"
                 placeholder="support@domain.xyz"
                 required
               />
               <TextField
                 className={styles.textField}
-                error={!!formErrors.base_contact_email}
-                helperText={formErrors.base_contact_email}
+                error={!!errorObj.base_contact_email}
+                helperText={errorObj.base_contact_email}
                 size="small"
                 name="base_contact_email"
                 label="Email to contact"
-                value={stepData.base_contact_email}
+                value={fields.base_contact_email}
                 type="email"
                 placeholder="tech@domain.xyz"
                 required
@@ -171,34 +114,34 @@ const Base = ({handleNextStep, step}) => {
               <Box sx={{marginTop: '8px'}} className={styles.formBlock}>
               <TextField
                 className={styles.textField}
-                error={!!formErrors.base_adpanel_host_prefix}
-                helperText={formErrors.base_adpanel_host_prefix}
+                error={!!errorObj.base_adpanel_host_prefix}
+                helperText={errorObj.base_adpanel_host_prefix}
                 size="small"
                 name="base_adpanel_host_prefix"
                 label="Adpanel host prefix"
-                value={stepData.base_adpanel_host_prefix}
+                value={fields.base_adpanel_host_prefix}
                 type="text"
                 required
               />
               <TextField
                 className={styles.textField}
-                error={!!formErrors.base_aduser_host_prefix}
-                helperText={formErrors.base_aduser_host_prefix}
+                error={!!errorObj.base_aduser_host_prefix}
+                helperText={errorObj.base_aduser_host_prefix}
                 size="small"
                 name="base_aduser_host_prefix"
                 label="Aduser host prefix"
-                value={stepData.base_aduser_host_prefix}
+                value={fields.base_aduser_host_prefix}
                 type="text"
                 required
               />
               <TextField
                 className={styles.textField}
-                error={!!formErrors.base_adserver_host_prefix}
-                helperText={formErrors.base_adserver_host_prefix}
+                error={!!errorObj.base_adserver_host_prefix}
+                helperText={errorObj.base_adserver_host_prefix}
                 size="small"
                 name="base_adserver_host_prefix"
                 label="Adserver host prefix"
-                value={stepData.base_adserver_host_prefix}
+                value={fields.base_adserver_host_prefix}
                 type="text"
                 required
               />
--- assets/installer/Views/InstallerSteps/Wallet/Wallet.jsx
+++ assets/installer/Views/InstallerSteps/Wallet/Wallet.jsx
@@ -1,129 +1,116 @@
 import React, { useEffect, useState } from 'react'
 import apiService from '../../../utils/apiService'
-import { Box, TextField, } from '@mui/material'
+import { Box, Button, TextField, Typography, } from '@mui/material'
 import WindowCard from '../../../Components/WindowCard/WindowCard'
 import Spinner from '../../../Components/Spiner/Spinner'
-import styles from '../styles.scss'
+import styles from './styles.scss'
+import { useForm } from '../../../hooks/hooks'
 
 const Wallet = ({handleNextStep, handlePrevStep, step}) => {
   const [isLoading, setIsLoading] = useState(true)
-  const [stepData, setStepData] = useState({
+  const {fields, errorObj, setFields, isFormValid, onFormChange, validate} = useForm({
     wallet_address: '',
     wallet_secret_key: '',
-    data_required: true
   })
-
-  const [formErrors, setFormErrors] = useState({
-    wallet_address: '',
-    wallet_secret_key: ''
-  })
-  const [isFormValid, setIsFormValid] = useState(false)
+  const [editMode, setEditMode] = useState(false)
 
   useEffect(() => {
     getStepData().catch(error => console.log(error))
 
   }, [])
 
-  useEffect(() => {
-    checkFormError()
-  }, [stepData])
-
   const getStepData = async () => {
     setIsLoading(true)
     const response = await apiService.getCurrentStepData(step.path)
-    setStepData({...stepData, ...response })
     setIsLoading(false)
-    console.log(response)
-  }
+    setFields({...fields, ...response})
+    setEditMode(response.data_required)
 
-  const checkFormError = () => {
-    const isFormErrors = Object.keys(formErrors).some(el => !!formErrors[el])
-    setIsFormValid(!isFormErrors)
-  }
-
-  const onFormChange = (e) => {
-    const {name, value} = e.target
-    console.log(name, value)
-    setStepData({...stepData, [name]: value})
-
-    if(!!value){
-      setFormErrors({...formErrors, [name]: ''})
-    } else {
-      setFormErrors({...formErrors, [name]: 'Required field'})
-    }
-    if(name === 'wallet_address'){
-      if(value.match(/^[0-9A-F]{4}-[0-9A-F]{8}-([0-9A-F]{4})$/g)){
-        setFormErrors({...formErrors, wallet_address: ''})
-      } else {
-        setFormErrors({...formErrors, wallet_address: 'invalid address format'})
-      }
-    }
-    if(name === 'wallet_secret_key'){
-      if(value.match(/^[0-9A-F]{64}$/g)){
-        setFormErrors({...formErrors, wallet_secret_key: ''})
-      } else {
-        setFormErrors({...formErrors, wallet_secret_key: 'invalid key format'})
-      }
-    }
   }
 
   const handleSubmit = async () => {
+    if(!editMode){
+      await apiService.sendStepData(step.path, {})
+      handleNextStep(step)
+      setIsLoading(false)
+      return
+    }
     if(!isFormValid) {
       return
     }
+    console.log(isFormValid)
     setIsLoading(true)
-    await apiService.sendStepData(step.path, stepData)
+    await apiService.sendStepData(step.path, fields)
     handleNextStep(step)
     setIsLoading(false)
   }
 
-  const onNextClick = () => {
-    // if(!!stepData.walletAddress){
-    //   handleNextStep(step)
-    // }
+  const conditionalComponent = (isEditMode) => {
+    return isEditMode ? (
+      <Box className={styles.container}>
+        <Box
+          component='form'
+          className={styles.container}
+          onChange={onFormChange}
+          onBlur={(e) => validate(e.target)}
+        >
+          <TextField
+            className={styles.textField}
+            error={!!errorObj.wallet_address}
+            helperText={errorObj.wallet_address}
+            value={fields.wallet_address}
+            margin='normal'
+            size='small'
+            name='wallet_address'
+            label='Wallet address'
+            type='text'
+            required
+          />
+          <TextField
+            error={!!errorObj.wallet_secret_key}
+            helperText={errorObj.wallet_secret_key}
+            value={fields.wallet_secret_key}
+            margin='normal'
+            size='small'
+            name='wallet_secret_key'
+            label='Wallet private key'
+            required
+          />
+          <Button
+            onClick={() => setEditMode(!editMode)}
+            className={(isEditMode && !fields.data_required) ? styles.visible : styles.hidden}
+          >
+            Cancel
+          </Button>
+        </Box>
+      </Box>
+    ) : (
+      <Box className={styles.container}>
+        <Box className={styles.content}>
+          <Typography>
+            Your wallet address: {fields.wallet_address}
+          </Typography>
+          <Button
+            onClick={() => setEditMode(!editMode)}
+            className={(isEditMode ? styles.hidden : styles.visible)}
+          >
+            Edit
+          </Button>
+        </Box>
+      </Box>
+    )
   }
 
   return (
     <WindowCard
       title='Wallet information'
       onNextClick={handleSubmit}
-      disabledNext={!isFormValid || isLoading}
+      disabledNext={editMode ? !isFormValid || isLoading : isLoading}
       onBackClick={() => handlePrevStep(step)}
     >
       {isLoading ?
         <Spinner/> :
-        <Box className={styles.container}>
-          <Box
-            component='form'
-            className={styles.container}
-            onChange={onFormChange}
-          >
-            <TextField
-              className={styles.textField}
-              error={!!formErrors.addressError}
-              helperText={formErrors.addressError}
-              value={stepData.wallet_address}
-              margin='normal'
-              size='small'
-              name='wallet_address'
-              label='Wallet address'
-              type='text'
-              required
-            />
-            <TextField
-              error={!!formErrors.keyError}
-              helperText={formErrors.keyError}
-              value={stepData.wallet_secret_key}
-              margin='normal'
-              size='small'
-              name='wallet_secret_key'
-              label='Wallet private key'
-              multiline
-              rows={2}
-              required
-            />
-          </Box>
-        </Box>
+        conditionalComponent(editMode)
       }
       </WindowCard>
   )
