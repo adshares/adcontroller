91c5e70a30c8d6957e014b0e32b6eef561cddc33
MykolaZhura <nikoyea14@gmail.com>
2022-07-06 16:13:17 +0200

    change classifier component according remarks

--- assets/installer/Views/InstallerSteps/Classifier/Classifier.jsx
+++ assets/installer/Views/InstallerSteps/Classifier/Classifier.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import apiService from '../../../utils/apiService'
 import WindowCard from '../../../Components/WindowCard/WindowCard'
-import { Box, Typography } from '@mui/material'
+import { Box, LinearProgress, Typography } from '@mui/material'
 import styles from './styles.scss'
 
 const Classifier = ({ handleNextStep, handlePrevStep, step }) => {
   const [isLoading, setIsLoading] = useState(true)
+  const [registrationInProgress, setRegistrationInProgress] = useState(false)
   const [stepData, setStepData] = useState({})
+  const [alert, setAlert] = useState({type: '', message: ''})
 
   useEffect(() => {
     getStepData().catch(error => console.log(error))
@@ -21,16 +23,27 @@ const Classifier = ({ handleNextStep, handlePrevStep, step }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    await apiService.sendStepData(step.path, {})
+    setRegistrationInProgress(true)
+    const response = await apiService.sendStepData(step.path, {})
+    if(response.code > 300){
+      setAlert({
+        type: 'error',
+        message: response.message
+      })
+      setRegistrationInProgress(false)
+      return
+    }
+    setRegistrationInProgress(false)
     handleNextStep(step)
   }
 
   return (
     <WindowCard
+      alert={alert}
       dataLoading={isLoading}
       title="Classifier information"
       onNextClick={handleSubmit}
-      disabledNext={false}
+      disabledNext={registrationInProgress || isLoading}
       onBackClick={() => handlePrevStep(step)}
     >
 
@@ -38,6 +51,11 @@ const Classifier = ({ handleNextStep, handlePrevStep, step }) => {
         <Typography variant="h5">
           Registration in AdClassify
         </Typography>
+        {registrationInProgress && (
+          <Box sx={{ width: '100%' }}>
+            <LinearProgress />
+          </Box>
+        )}
       </Box>
 
     </WindowCard>
