f84aa8c7043d4e7364bb0cefb49b92c5eb55fdb2
MykolaZhura <nikoyea14@gmail.com>
2022-07-12 12:43:07 +0200

    change login page view

--- assets/installer/Views/Login/Login.jsx
+++ assets/installer/Views/Login/Login.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from 'react'
 import apiService from '../../utils/apiService'
-import { Button, TextField } from '@mui/material'
-import WindowCard from '../../Components/WindowCard/WindowCard'
+import {
+  Alert,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Collapse, IconButton,
+  Link,
+  TextField,
+  Typography
+} from '@mui/material'
+import styles from './styles.scss'
+import logo from '../../../img/logo.png'
+import CloseIcon from '@mui/icons-material/Close'
 
 export default function Login ({ setToken }) {
   const [email, setEmail] = useState('')
@@ -30,6 +43,8 @@ export default function Login ({ setToken }) {
   const handleInputChange = e => {
     const { name, value } = e.target
 
+    setAlert({type: '', message: '', title: ''})
+
     switch (name) {
       case 'email':
         setEmail(value)
@@ -45,43 +60,88 @@ export default function Login ({ setToken }) {
   }
 
   return (
-    <WindowCard
-      alert={alert}
-      title="Please login"
-      isFirstCard
-      isLastCard
-    >
-      <form onSubmit={handleSubmit}>
-        <TextField
-          variant="standard"
-          margin="normal"
-          required
-          fullWidth
-          type="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          onChange={handleInputChange}
+    <Card className={styles.container}>
+      <CardContent className={styles.logo}>
+        <Box
+          component='img'
+          src={logo}
+          height='70px'
+          width='70px'
         />
-        <TextField
-          variant="standard"
-          margin="normal"
-          required
-          fullWidth
-          type="password"
-          label="Password"
-          name="password"
-          onChange={handleInputChange}
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
+      </CardContent>
+      <CardHeader
+        title='Please login'
+        titleTypographyProps={{align: 'center'}}
+      />
+
+      <CardContent>
+        <Box
+          onSubmit={handleSubmit}
+          component='form'
         >
-          Login
-        </Button>
-      </form>
-    </WindowCard>
+          <TextField
+            variant="standard"
+            margin="normal"
+            required
+            fullWidth
+            type="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            onChange={handleInputChange}
+          />
+          <TextField
+            variant="standard"
+            margin="normal"
+            required
+            fullWidth
+            type="password"
+            label="Password"
+            name="password"
+            onChange={handleInputChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+        </Box>
+        <Box className={styles.link}>
+          <Link
+            href='https://web3ads.net/auth/forgotten-password'
+            variant='body1'
+            underline='hover'
+            target='_blank'
+            align='center'
+          >
+            Forgot password?
+          </Link>
+        </Box>
+      </CardContent>
+      <Collapse in={!!alert.title}>
+          <Alert
+            severity={alert.type}
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setAlert({type: '', message: '', title: ''});
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+          >
+            <Typography variant='body2'>
+              {alert.title}: {alert.message}
+            </Typography>
+          </Alert>
+      </Collapse>
+    </Card>
   )
 }
