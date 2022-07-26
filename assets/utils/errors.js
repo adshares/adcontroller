779076e0197a3ca335dac650b7d4231934577a88
MykolaZhura <nikoyea14@gmail.com>
2022-07-26 14:32:44 +0200

    fix changed __proto__

--- assets/utils/errors.js
+++ assets/utils/errors.js
@@ -1,6 +1,5 @@
 export function HttpError(message, data) {
-  // eslint-disable-next-line no-proto
-  this.constructor.prototype.__proto__ = Error.prototype;
+  Object.setPrototypeOf(this.constructor.prototype, Error.prototype);
   if (Error.captureStackTrace) {
     Error.captureStackTrace(this, this.constructor);
   }
