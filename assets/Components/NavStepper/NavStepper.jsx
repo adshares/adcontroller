e904054d5d7ac04e2569475b9cb151aef337807a
MykolaZhura <nikoyea14@gmail.com>
2022-08-04 14:09:19 +0200

    fix stepper component as review notes

--- assets/Components/NavStepper/NavStepper.jsx
+++ assets/Components/NavStepper/NavStepper.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Box, Step, Stepper, StepButton } from '@mui/material';
+import { Box, Step, Stepper, StepButton, Typography } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 
 const NavStepper = ({ steps, unlockedSteps, handleCurrentStep }) => {
   const completedSteps = unlockedSteps.filter((step) => step.index < unlockedSteps.length).map((step) => step.path);
+  const location = useLocation();
+  console.log(location.pathname.slice(7));
+  console.log(unlockedSteps);
 
   const items = steps.map((step) => {
+    const activeStep = step.path === location.pathname.slice(7);
     return (
       <Step
         key={step.index}
@@ -16,7 +21,9 @@ const NavStepper = ({ steps, unlockedSteps, handleCurrentStep }) => {
           sx={{ textTransform: 'capitalize', padding: '16px', margin: '-16px', borderRadius: '8px' }}
           onClick={() => handleCurrentStep(step)}
         >
-          {step.path}
+          <Typography variant="body2" sx={activeStep && { fontWeight: 600, color: 'primary.main' }}>
+            {step.path === 'smtp' ? step.path.toUpperCase() : step.path}
+          </Typography>
         </StepButton>
       </Step>
     );
