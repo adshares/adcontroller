f8093d315d9617a8409292e0a26812d127ae20e1
Mykola <73442621+Niko-Yea@users.noreply.github.com>
2022-08-10 15:52:09 +0200

    Fix classifier (#15)
    * add form to classifier component
    
    * fix warning in console
    
    * classifier handle form error

--- assets/AppInstaller/InstallerSteps/Classifier/Classifier.jsx
+++ assets/AppInstaller/InstallerSteps/Classifier/Classifier.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Box, LinearProgress, Typography } from '@mui/material';
+import { Box, Button, LinearProgress, TextField, Typography } from '@mui/material';
 import apiService from '../../../utils/apiService';
 import InstallerStepWrapper from '../../../Components/InstallerStepWrapper/InstallerStepWrapper';
 import styles from './styles.scss';
+import { useForm } from '../../../hooks';
 
 function Classifier({ handleNextStep, handlePrevStep, step }) {
+  const { fields, onFormChange, errorObj, isFormValid } = useForm({ apiKey: '', apiSecret: '' });
   const [isLoading, setIsLoading] = useState(true);
   const [registrationInProgress, setRegistrationInProgress] = useState(false);
   const [stepData, setStepData] = useState({});
   const [alert, setAlert] = useState({ type: '', message: '' });
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     getStepData();
@@ -52,17 +55,54 @@ function Classifier({ handleNextStep, handlePrevStep, step }) {
       dataLoading={isLoading}
       title="Classifier information"
       onNextClick={handleSubmit}
-      disabledNext={registrationInProgress || isLoading}
+      disabledNext={registrationInProgress || isLoading || (editMode && !isFormValid)}
       onBackClick={() => handlePrevStep(step)}
     >
       <Box className={styles.container}>
-        <Typography gutterBottom ma variant="h6">
+        <Typography align="center" gutterBottom variant="h6">
           Registration in AdClassify
         </Typography>
         <Typography variant="body1" paragraph align="justify">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, accusamus amet aperiam architecto beatae doloremque est et explicabo
           harum id odio officia quae quas quod sint temporibus vitae voluptas voluptatem.
         </Typography>
+        <Box className={styles.editButtonThumb}>
+          <Button onClick={() => setEditMode((prevState) => !prevState)} type="button">
+            {editMode ? 'Cancel' : 'Already have an account'}
+          </Button>
+        </Box>
+        {editMode && (
+          <Box className={styles.formThumb}>
+            <TextField
+              sx={{ width: '50%' }}
+              error={!!errorObj.apiKey}
+              helperText={errorObj.apiKey}
+              size="small"
+              name="apiKey"
+              label="Api key"
+              margin="dense"
+              value={fields.apiKey}
+              onChange={onFormChange}
+              type="text"
+              fullWidth
+              inputProps={{ autoComplete: 'off' }}
+            />
+            <TextField
+              sx={{ width: '50%' }}
+              error={!!errorObj.apiSecret}
+              helperText={errorObj.apiSecret}
+              size="small"
+              name="apiSecret"
+              label="Api secret"
+              margin="dense"
+              value={fields.apiSecret}
+              onChange={onFormChange}
+              type="text"
+              fullWidth
+              inputProps={{ autoComplete: 'off' }}
+            />
+          </Box>
+        )}
         {registrationInProgress && (
           <Box sx={{ width: '100%' }}>
             <LinearProgress />
