c020675b38ba51a86aef47eea72ef71bdcda1596
MykolaZhura <nikoyea14@gmail.com>
2022-08-10 15:32:01 +0200

    add form to classifier component

--- assets/AppInstaller/InstallerSteps/Classifier/Classifier.jsx
+++ assets/AppInstaller/InstallerSteps/Classifier/Classifier.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Box, LinearProgress, Typography } from '@mui/material';
+import { Box, Button, LinearProgress, TextField, Typography } from '@mui/material';
 import apiService from '../../../utils/apiService';
 import InstallerStepWrapper from '../../../Components/InstallerStepWrapper/InstallerStepWrapper';
 import styles from './styles.scss';
+import { useForm } from '../../../hooks';
 
 function Classifier({ handleNextStep, handlePrevStep, step }) {
+  const { fields } = useForm({ apiKey: '', apiSecret: '' });
   const [isLoading, setIsLoading] = useState(true);
   const [registrationInProgress, setRegistrationInProgress] = useState(false);
   const [stepData, setStepData] = useState({});
   const [alert, setAlert] = useState({ type: '', message: '' });
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     getStepData();
@@ -56,16 +59,49 @@ function Classifier({ handleNextStep, handlePrevStep, step }) {
       onBackClick={() => handlePrevStep(step)}
     >
       <Box className={styles.container}>
-        <Typography gutterBottom ma variant="h6">
+        <Typography align="center" gutterBottom ma variant="h6">
           Registration in AdClassify
         </Typography>
         <Typography variant="body1" paragraph align="justify">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, accusamus amet aperiam architecto beatae doloremque est et explicabo
           harum id odio officia quae quas quod sint temporibus vitae voluptas voluptatem.
         </Typography>
+        {editMode && (
+          <Box className={styles.formThumb}>
+            <TextField
+              sx={{ width: '50%' }}
+              size="small"
+              name="apiKey"
+              label="Your api key"
+              margin="dense"
+              value={fields.apiKey}
+              type="text"
+              fullWidth
+              inputProps={{ autoComplete: 'off' }}
+            />
+            <TextField
+              sx={{ width: '50%' }}
+              size="small"
+              name="apiSecret"
+              label="Your api secret"
+              margin="dense"
+              value={fields.apiSecret}
+              type="text"
+              fullWidth
+              inputProps={{ autoComplete: 'off' }}
+            />
+          </Box>
+        )}
         {registrationInProgress && (
           <Box sx={{ width: '100%' }}>
             <LinearProgress />
+          </Box>
+        )}
+        {!editMode && (
+          <Box className={styles.editButtonThumb}>
+            <Button onClick={() => setEditMode(true)} type="button">
+              Already have api key
+            </Button>
           </Box>
         )}
       </Box>
