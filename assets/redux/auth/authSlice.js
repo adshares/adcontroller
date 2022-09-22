edcb1cb998466666ffb53a3d6a6121feaebd8cad
MykolaZhura <nikoyea14@gmail.com>
2022-09-22 23:34:11 +0200

    create dialog with synchronization information

--- assets/redux/auth/authSlice.js
+++ assets/redux/auth/authSlice.js
@@ -10,23 +10,14 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     setAppLogin: (state, { payload }) => {
-      localStorage.setItem('authToken', payload);
       state.token = payload;
       state.isLoggedIn = true;
     },
     setAppLogout: (state) => {
-      localStorage.removeItem('authToken');
       state.token = null;
       state.isLoggedIn = false;
     },
     checkAppAuth: (state) => {
-      const token = localStorage.getItem('authToken');
-      if (!token) {
-        state.token = null;
-        state.isLoggedIn = false;
-        return;
-      }
-      state.token = token;
       state.isLoggedIn = true;
     },
   },
