5f9ee081d051067029b4d766d8d15a81699ae261
MykolaZhura <nikoyea14@gmail.com>
2022-09-27 15:04:09 +0200

    refactor

--- assets/Components/ListOfInputs/ListOfInputs.jsx
+++ assets/Components/ListOfInputs/ListOfInputs.jsx
@@ -1,28 +1,29 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { validateAddress } from '@adshares/ads';
 import { Box, Button, Collapse, IconButton, InputAdornment, List, ListItem, TextField } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import commonStyles from '../../styles/commonStyles.scss';
+import { compareArrays } from '../../utils/helpers';
+
+const sliceDomain = (value) => {
+  const isValidUrl = (() => {
+    try {
+      new URL(value);
+    } catch {
+      sliceDomain('https://' + value);
+      return false;
+    }
+    return true;
+  })();
+
+  if (isValidUrl) {
+    const url = new URL(value);
+    return url.hostname;
+  }
+  return value;
+};
 
 const formatValue = (value, type) => {
-  const sliceDomain = (value) => {
-    const isValidUrl = (() => {
-      try {
-        new URL(value);
-      } catch {
-        sliceDomain('https://' + value);
-        return false;
-      }
-      return true;
-    })();
-
-    if (isValidUrl) {
-      const url = new URL(value);
-      return url.hostname;
-    }
-    return value;
-  };
-
   switch (type) {
     case 'domain':
       return sliceDomain(value);
@@ -87,41 +88,17 @@ const validateValue = (list, value, type) => {
   }
 };
 
-const compareArray = (arr1, arr2) => {
-  return Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
-};
-
-export default function ListOfInputs({ initialList = null, fieldsHandler, listName = undefined, type = 'text', maxHeight = '50vh' }) {
+const useCreateFields = (initialList, type) => {
   const [list, setList] = useState(initialList || []);
-  const [inputs, setInputs] = useState([]);
-  const [bulkAdding, setBulkAdding] = useState(false);
-  const [textareaValue, setTextareaValue] = useState('');
-  const textAreaRef = useRef(null);
 
   useEffect(() => {
-    const { inputs, fields } = createFields(list);
-    setInputs(inputs);
-    const createdList = fields.map((field) => field.field);
-    const isValuesValid = fields.every((field) => field.isValueValid);
-    const isListWasChanged = !compareArray(initialList || [], createdList);
-    fieldsHandler({
-      listName,
-      isValuesValid,
-      isListWasChanged,
-      createdList,
-    });
-  }, [list, initialList]);
+    if (!compareArrays(initialList || [], list)) {
+      setList(initialList || []);
+    }
+  }, [initialList]);
 
-  const onTextareaChange = (e) => {
-    setTextareaValue(e.target.value);
-  };
-
-  const onSaveClick = () => {
-    const newList = textareaValue.split(/[,;\s]/).filter(Boolean);
-    setList((prevState) => [...prevState, ...newList.map((item) => formatValue(item, type))]);
-    setTextareaValue('');
-    setBulkAdding((prevState) => !prevState);
-  };
+  const inputs = [];
+  const fields = [];
 
   const handleChange = (e, idx) => {
     setList((prevState) => {
@@ -140,9 +117,11 @@ export default function ListOfInputs({ initialList = null, fieldsHandler, listNa
   };
 
   const formatValueOnBlur = (e, index) => {
+    if (type === 'wallet') return;
     if (!e.target.value) {
       return;
     }
+
     setList((prevState) => {
       const newList = [...prevState];
       newList[index] = formatValue(e.target.value, type);
@@ -150,49 +129,74 @@ export default function ListOfInputs({ initialList = null, fieldsHandler, listNa
     });
   };
 
-  const createFields = (list) => {
-    const inputs = [];
-    const fields = [];
-    for (let index = 0; index <= list.length; index++) {
-      const validationResult = list.length !== index && validateValue(list, list[index], type);
-      const isValidationError = list.length !== index && !validationResult.isValueValid;
+  for (let index = 0; index <= list.length; index++) {
+    const validationResult = list.length !== index && validateValue(list, list[index], type);
+    const isValidationError = list.length !== index && !validationResult.isValueValid;
 
-      if (validationResult) {
-        fields.push({ field: list[index], ...validationResult });
-      }
-
-      inputs.push(
-        <ListItem disableGutters disablePadding key={index}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            size="small"
-            margin="dense"
-            error={isValidationError}
-            helperText={validationResult.helperText}
-            name={`${index}`}
-            value={list[index] || ''}
-            onChange={(e) => handleChange(e, index)}
-            onBlur={(e) => formatValueOnBlur(e, index)}
-            InputProps={
-              index === list.length
-                ? undefined
-                : {
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton type="button" onClick={() => onRemoveClick(index)}>
-                          <CloseIcon color="error" />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }
-            }
-            inputProps={{ autoComplete: 'off' }}
-          />
-        </ListItem>,
-      );
+    if (validationResult) {
+      fields.push({ field: list[index], ...validationResult });
     }
-    return { inputs: inputs.reverse(), fields };
+
+    inputs.push(
+      <ListItem disableGutters disablePadding key={index}>
+        <TextField
+          fullWidth
+          variant="outlined"
+          size="small"
+          margin="dense"
+          error={isValidationError}
+          helperText={validationResult.helperText}
+          name={`${index}`}
+          value={list[index] || ''}
+          onChange={(e) => handleChange(e, index)}
+          onBlur={(e) => formatValueOnBlur(e, index)}
+          InputProps={
+            index === list.length
+              ? undefined
+              : {
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton type="button" onClick={() => onRemoveClick(index)}>
+                        <CloseIcon color="error" />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }
+          }
+          inputProps={{ autoComplete: 'off' }}
+        />
+      </ListItem>,
+    );
+  }
+  return { inputs, fields, list, setList };
+};
+
+export default function ListOfInputs({ initialList = null, fieldsHandler, listName = undefined, type = 'text', maxHeight = undefined }) {
+  const { inputs, fields, list, setList } = useCreateFields(initialList, type);
+  const [bulkAdding, setBulkAdding] = useState(false);
+  const [textareaValue, setTextareaValue] = useState('');
+  const textAreaRef = useRef(null);
+  const isValuesValid = useMemo(() => fields.every((field) => field.isValueValid), [fields]);
+  const isListWasChanged = useMemo(() => !compareArrays(initialList || [], list), [initialList, list]);
+
+  useEffect(() => {
+    fieldsHandler({
+      listName,
+      isValuesValid,
+      isListWasChanged,
+      list,
+    });
+  }, [list, initialList]);
+
+  const onTextareaChange = (e) => {
+    setTextareaValue(e.target.value);
+  };
+
+  const onTextAreaSaveClick = () => {
+    const newList = textareaValue.split(/[,;\s]/).filter(Boolean);
+    setList((prevState) => [...prevState, ...newList.map((item) => formatValue(item, type))]);
+    setTextareaValue('');
+    setBulkAdding((prevState) => !prevState);
   };
 
   return (
@@ -204,14 +208,14 @@ export default function ListOfInputs({ initialList = null, fieldsHandler, listNa
           </Button>
         </Box>
 
-        <List sx={{ overflow: 'auto', maxHeight: maxHeight, minHeight: '5rem' }}>{inputs}</List>
+        <List sx={{ overflow: 'auto', maxHeight: maxHeight, minHeight: '5rem' }}>{inputs.reverse()}</List>
       </Collapse>
 
       <Collapse in={bulkAdding} timeout="auto">
         <Box className={`${commonStyles.card} ${commonStyles.flex} ${commonStyles.flexColumn}`}>
           <TextField value={textareaValue} multiline rows={8} onChange={onTextareaChange} inputRef={textAreaRef} />
           <Box className={`${commonStyles.card} ${commonStyles.flex} ${commonStyles.justifyFlexEnd}`}>
-            <Button type="button" variant="contained" sx={{ mr: 1 }} onClick={onSaveClick} disabled={!textareaValue}>
+            <Button type="button" variant="contained" sx={{ mr: 1 }} onClick={onTextAreaSaveClick} disabled={!textareaValue}>
               Paste
             </Button>
 
