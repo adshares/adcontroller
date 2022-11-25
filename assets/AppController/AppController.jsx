8b11905bd84540087acd0ad2a651dce2eafe4423
MykolaZhura <nikoyea14@gmail.com>
2022-11-25 11:09:47 +0100

    fix main app container width

--- assets/AppController/AppController.jsx
+++ assets/AppController/AppController.jsx
@@ -237,7 +237,7 @@ function AppController() {
   return (
     <Box className={`${commonStyles.flex}`}>
       <SideMenu enableSideMenu={isLoggedIn} showSideMenu={showSideMenu} toggleSideMenu={toggleSideMenu} menuItems={appModules} />
-      <Box className={`${commonStyles.flex} ${commonStyles.flexColumn}`} sx={{ width: '100%' }}>
+      <Box className={`${commonStyles.flex} ${commonStyles.flexColumn}`} sx={{ flexGrow: 1, width: 'calc(100% - 292px)' }}>
         <MenuAppBar showProtectedOptions={isLoggedIn} showSideMenu={showSideMenu} toggleSideMenu={toggleSideMenu} showSideMenuIcon />
         <AppWindow>
           <SynchronizationDialog
