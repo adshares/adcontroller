7b31a00eab3c7792814463ed1bda28d5ad075b62
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2023-03-14 13:12:17 +0100

    Revert "Light and dark var"

--- assets/Components/NavStepper/NavStepper.jsx
+++ assets/Components/NavStepper/NavStepper.jsx
@@ -14,10 +14,10 @@ const Step = styled(MuiStep)(({ theme, disabled, active, current }) => {
       '& circle': {
         r: 11,
         fill: disabled ? '#7b7b7b' : current ? theme.palette.primary.main : 'transparent',
-        stroke: active && !current ? theme.palette.dark.main : 'transparent',
+        stroke: active && !current ? theme.palette.black.main : 'transparent',
       },
       '& text': {
-        fill: active && !current ? theme.palette.dark.main : theme.palette.light.main,
+        fill: active && !current ? theme.palette.black.main : theme.palette.white.main,
         stroke: 'none',
         fontVariationSettings: !current && !disabled ? '"wght" 900' : '"wght" 400',
       },
