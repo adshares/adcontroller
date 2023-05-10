904c96f3f1b46c6f0e93ab6fff4f833f12f6bf2e
Mykola <73442621+Niko-Yea@users.noreply.github.com>
2023-05-10 10:26:09 +0200

    It 421 favicon (#133)
    * opportunity to change controller favicon
    
    * fix review remarks
    
    ---------
    
    Co-authored-by: Maciej Pilarczyk <m.pilarczyk@outlook.com>

---
+++ src/Messenger/MessageHandler/AdControllerReloadHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Messenger\MessageHandler;
+
+use App\Exception\UnexpectedResponseException;
+use App\Messenger\Message\AdControllerReload;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Process\Process;
+
+#[AsMessageHandler]
+class AdControllerReloadHandler
+{
+    private const TIMEOUT = 15 * 60;
+
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    public function __invoke(AdControllerReload $message): void
+    {
+        $this->reload();
+    }
+
+    private function reload(): void
+    {
+        $process = new Process(['deploy/reload.sh']);
+        $process->setTimeout(self::TIMEOUT);
+        $process->run();
+        $process->wait();
+
+        if (!$process->isSuccessful()) {
+            $this->logger->error(
+                sprintf('AdController reload failed (std): %s', self::formatOutput($process->getOutput()))
+            );
+            $this->logger->error(
+                sprintf('AdController reload failed (err): %s', self::formatOutput($process->getErrorOutput()))
+            );
+            throw new UnexpectedResponseException('AdController reload failed');
+        }
+    }
+
+    private static function formatOutput(string $message): string
+    {
+        return preg_replace('/\s+/', ' ', $message);
+    }
+}
