e28c158c642712b0e50e5269009050981631abed
MykolaZhura <nikoyea14@gmail.com>
2023-06-05 20:27:09 +0200

    fetch media/vendors/taxonomy

--- assets/AppController/Supply/Placeholders.jsx
+++ assets/AppController/Supply/Placeholders.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
-import { Card, CardContent, CardHeader } from '@mui/material';
-import queryString from 'query-string';
+import React, { useEffect, useState } from 'react';
+import { Box, Card, CardContent, CardHeader, FormControl, InputLabel, MenuItem } from '@mui/material';
+import { useGetMediaQuery, useGetVendorListQuery, useGetTaxonomyQuery } from '../../redux/taxonomy/taxonomyApi';
+import Spinner from '../../Components/Spinner/Spinner';
+import { Select } from '@mui/material';
+import commonStyles from '../../styles/commonStyles.scss';
 
 export default function Placeholders() {
   return (
@@ -12,50 +15,44 @@ export default function Placeholders() {
 
 const PlaceholdersCard = (props) => {
   const [fileList, setFileList] = useState(null);
+  const [selectedMedium, setMedium] = useState({
+    medium: null,
+    vendor: null,
+  });
+  const { data: media, isFetching: isMediaFetching, isSuccess: isMediaFetchingSuccess } = useGetMediaQuery();
+  const {
+    data: vendorList,
+    isFetching: isVendorListFetching,
+    isSuccess: isVendorListFetchingSuccess,
+  } = useGetVendorListQuery(
+    { medium: selectedMedium.medium },
+    {
+      refetchOnMountOrArgChange: true,
+      skip: !selectedMedium.medium || selectedMedium.medium === 'web',
+    },
+  );
+  const { data: taxonomy, isLoading: isTaxonomyLoading } = useGetTaxonomyQuery(selectedMedium, {
+    refetchOnMountOrArgChange: true,
+    skip: !selectedMedium.medium || (selectedMedium.medium === 'metaverse' && !selectedMedium.vendor),
+  });
 
-  // // media list
-  // fetch('http://localhost:8030/api/taxonomy/media')
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data))
-  //   .catch((err) => console.error(err));
-  //
-  // // vendors list for medium=decentraland
-  // fetch('http://localhost:8030/api/taxonomy/media/decentraland/vendors')
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data))
-  //   .catch((err) => console.error(err));
-  //
-  // // taxonomy for medium=metaverse, vendor=decentraland
-  // fetch('http://localhost:8030/api/taxonomy/media/metaverse?vendor=decentraland')
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data))
-  //   .catch((err) => console.error(err));
-  //
-  // // placeholders
-  // fetch('http://localhost:8030/api/supply-placeholders')
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data))
-  //   .catch((err) => console.error(err));
-  //
-  // const parsed = {
-  //   'filter[medium]': 'metaverse',
-  //   'filter[vendor]': 'decentraland',
-  // };
-  // const query = queryString.stringify(parsed);
-  // // placeholders with filter
-  // fetch(`http://localhost:8030/api/supply-placeholders?${query}`)
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data))
-  //   .catch((err) => console.error(err));
-  //
-  // const uuid = 'e1ef118b-c0a1-49f8-8f77-244a8b04b553';
-  // // delete placeholder
-  // fetch(`http://localhost:8030/api/supply-placeholders/${uuid}`, {
-  //   method: 'DELETE',
-  // })
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data))
-  //   .catch((err) => console.error(err));
+  useEffect(() => {
+    if (media?.data?.data) {
+      setMedium((prevState) => ({
+        ...prevState,
+        medium: Object.keys(media.data.data).find((medium) => medium === 'web') || Object.keys(media.data.data)[0],
+      }));
+    }
+  }, [media]);
+
+  useEffect(() => {
+    if (selectedMedium.medium !== 'web' && vendorList?.data?.data) {
+      setMedium((prevState) => ({
+        ...prevState,
+        vendor: Object.keys(vendorList.data.data).find((vendor) => vendor === 'decentraland') || Object.keys(vendorList.data.data)[0],
+      }));
+    }
+  }, [vendorList, selectedMedium.medium]);
 
   const handleFileChange = (event) => {
     setFileList(event.target.files);
@@ -90,6 +87,65 @@ const PlaceholdersCard = (props) => {
     <Card {...props}>
       <CardHeader title="Placeholders" subheader="Set creatives which will be presented to the user in case of missing campaigns." />
       <CardContent>
+        <Box className={`${commonStyles.flex}`}>
+          {isMediaFetching && <Spinner />}
+
+          {selectedMedium.medium && isMediaFetchingSuccess && (
+            <Box sx={{ minWidth: '14.5rem', mr: 3 }}>
+              <FormControl fullWidth customvariant="highLabel">
+                <InputLabel id="selectMedium">Medium</InputLabel>
+                <Select
+                  id="selectMedium"
+                  color="secondary"
+                  value={selectedMedium.medium}
+                  onChange={(e) =>
+                    setMedium((prevState) => ({
+                      ...prevState,
+                      medium: e.target.value,
+                      ...(e.target.value === 'web' ? { vendor: null } : {}),
+                    }))
+                  }
+                >
+                  {Object.entries(media.data.data).map((medium) => (
+                    <MenuItem key={medium[0]} value={medium[0]}>
+                      {medium[1]}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          )}
+
+          {selectedMedium.vendor && (
+            <Box sx={{ minWidth: '14.5rem', mr: 3 }} className={`${commonStyles.flex} ${commonStyles.alignCenter}`}>
+              {isVendorListFetching ? (
+                <Spinner sx={{ minWidth: '14.5rem', mr: 3, mt: 2 }} />
+              ) : (
+                <FormControl fullWidth customvariant="highLabel">
+                  <InputLabel id="selectVendor">Vendor</InputLabel>
+                  <Select
+                    id="selectVendor"
+                    color="secondary"
+                    value={selectedMedium.vendor}
+                    onChange={(e) =>
+                      setMedium((prevState) => ({
+                        ...prevState,
+                        vendor: e.target.value,
+                      }))
+                    }
+                  >
+                    {Object.entries(vendorList.data.data).map((vendor) => (
+                      <MenuItem key={vendor[0]} value={vendor[0]}>
+                        {vendor[1]}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              )}
+            </Box>
+          )}
+        </Box>
+
         <div>
           <input type="file" onChange={handleFileChange} multiple />
           <ul>
